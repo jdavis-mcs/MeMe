0feb6f74472843a990efd40eba646e35453ced51
jdavis-mcs <jdavis@madison.k12.in.us>
2025-12-15 10:32:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -47,10 +47,26 @@ const deck = [
 // Reliable Image Placeholders (To ensure images definitely show up)
 // In production, you can replace these with real image URLs later.
 const memeImages = [
-    "https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png",
-    "https://upload.wikimedia.org/wikipedia/commons/e/e3/Buddy_christ.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/f/ff/Deep_in_thought.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/3/3b/Paris_Tuileries_Garden_Facepalm_statue.jpg"
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
+    "13.jpg",
+    "14.jpg",
+    "15.jpg",
+    "16.jpg",
+    "17.jpg",
+    "18.jpg",
+    "19.jpg",
+    "20.jpg"
 ];
 
 io.on('connection', (socket) => {
@@ -138,4 +154,5 @@ const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
 
